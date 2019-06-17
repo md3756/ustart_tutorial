ad4d92403cc22eff659a6ee5d32e35a9b6d1cc13
Mithila Das <md3756@nyu.edu>
2019-06-17 17:15:44 -0400

    small changes

--- car/storage/storage.go
+++ car/storage/storage.go
@@ -2,14 +2,12 @@ package storage
 
 import (
 	"context"
-
-	"github.com/md3756/ustart_tutorial/car/carpb"
 )
 
 // Storage is a database-agnostic interface for persisting customer data
 type Storage interface {
 	Register(context.Context, string, string, string, string, string, string) error
-	ToggleAvailable(context.Context, string) (carpb.Car, error)
+	ToggleAvailable(context.Context, string, bool) error
 	Search(context.Context, []string, bool, map[string][]string, string) ([]string, error)
 	// rest of the functions
 	ErrCarDoesNotExist() error
