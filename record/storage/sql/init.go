8b4d9054bdc33006b2b95263269c548539a4080a
Mithila Das <md3756@nyu.edu>
2019-06-19 16:09:30 -0400

    small change

--- record/storage/sql/init.go
+++ record/storage/sql/init.go
@@ -3,7 +3,7 @@ package sqlstore
 import "context"
 
 func (dbConn *SQLStore) Init(ctx context.Context) error {
-	-, err := dbConn.db.QueryContext(ctx, `CREATE TABLE IF NOT EXIST`, `dbConn.db.recordTableName
+	-, err := dbConn.db.QueryContext(ctx, `CREATE TABLE IF NOT EXIST `dbConn.db.recordTableName`
 	carID text NOT NULL,
 	userID text NOT NULL,
 	dateStart text NOT NULL,
